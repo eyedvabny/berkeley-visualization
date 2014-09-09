d6d86d144eb31c84b01b46a66e2e1072e8ef4ea6
eyedvabny <eugene@yedvabny.com>
2014-09-09 16:15:38 -0700

    Logistic updates to README and gitignore
    Added links to the course site and blocked .Rproj files from being added as
    they are generic across RStudio.

--- PS1_Visualization_Design/disasters.R
+++ PS1_Visualization_Design/disasters.R
@@ -6,20 +6,45 @@ library(ggplot2)
 # (http://vis.berkeley.edu/courses/cs294-10-fa14/wiki/images/8/8d/Disasters_killed_over_100.csv.zip)
 disasters <- read.csv('disasters_killed_over100.csv',stringsAsFactors=FALSE)
 
-#
+# Some data cleaning
+disasters <- within(disasters,{
+    # Numbers are too high and cause an overflow; reduce
+    Cost_dollars <- Cost_dollars/1000
+
+    # As some others have pointed out, this really means Famine
+    Type[Type=='Complex Disasters'] <- 'Famine'
+
+    # Apparently Tsunamis cause earthquakes
+    Type[Sub_Type == 'Earthquake (ground shaking)'] <- 'Earthquake'
+    Type[Sub_Type == 'Tsunami'] <- 'Tsunami'
+
+    # Extreme temperatures have two extremes
+    Type[Sub_Type == 'Cold wave'] <- 'Extreme Cold'
+    Type[Sub_Type == 'Extreme winter conditions'] <- 'Extreme Cold'
+    Type[Sub_Type == 'Heat wave'] <- 'Extreme Hot'
+
+    # Really stupid name for a disaster; most are landslides
+    Type[grep("mass movement",Type,ignore.case = TRUE)] <- 'Landslide'
+
+    # Group all accidents
+    Type[grep("accident",Type,ignore.case = TRUE)] <- 'Accident'
+
+
+})
+
+test <- arrange(disasters,Type,Sub_Type)
+
+# We want to do some summarizing
 disaster_impact <- disasters %>%
-  group_by(Type,Country) %>%
-  summarize(
-    deathToll = sum(Killed,na.rm=TRUE),
-    dollarCost = sum(Cost_dollars,na.rm=TRUE)
-  ) %>%
-  filter(Type == 'Flood')
-  mutate(
-    totCost = sum(dollarCost)
-  )
-
-
-mutate(
-  totCost = sum(Cost_dollars,na.rm=TRUE),
-  totDeath = sum(Killed,na.rm=TRUE)) %>%
-  arrange(Type,desc(totCost))
+    group_by(Type,Country) %>%
+    summarize(
+        deathToll = sum(Killed,na.rm=TRUE),
+        dollarCost = sum(Cost_dollars,na.rm=TRUE)
+    ) %>%
+    mutate(
+        humanCost = sum(deathToll),
+        materialCost = sum(dollarCost)
+    ) %>%
+    arrange(
+        desc(humanCost),desc(deathToll)
+    )
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 CS294-10: Visualization
 =======================
-This repository will contain personal course work for Berkeley CS294-10, Visualization, taught by Maneesh Argrawala in Fall 2014.
+This repository will contain personal course work for [Berkeley CS294-10](http://vis.berkeley.edu/courses/cs294-10-fa14/wiki/index.php/Main_Page), Visualization, taught by [Maneesh Argrawala](http://vis.berkeley.edu/~maneesh/) in Fall 2014.
 
 All code is independent work and &copy;Eugene Yedvabny
