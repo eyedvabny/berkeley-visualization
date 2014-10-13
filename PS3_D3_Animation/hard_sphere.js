215f777bf76f360e074c7c42e54304ea83f58f43
eyedvabny <eugene@yedvabny.com>
2014-10-12 18:29:29 -0700

    Exclusive selection of a particle

--- PS3_D3_Animation/hard_sphere.js
+++ PS3_D3_Animation/hard_sphere.js
@@ -10,6 +10,7 @@ var dim = 500; // Dimension of the canvas (square)
 //
 var particles = []; // Particles to plot on the canvas
 var frame; // The canvas element
+var root; // Currently selected particle
 
 //
 // Classes
@@ -18,30 +19,21 @@ var frame; // The canvas element
 // Particle class
 var Particle = function (){
 
-    // Generate a trial random position
-    var temp_x;
-    var temp_y;
+    var temp_x, temp_y;
 
-    // Check if two elements are collided
-    function is_collided(x,y){
-    	return function(elem){
-    		return (4*radius*radius) < (elem.x-x)*(elem.x-x) + (elem.y-y)*(elem.y-y);
-    	}
-    }
-
-    // Loop until we are not blocked
     while(true){
 
     	// Generate a trial position
     	temp_x = Math.random() * (dim-2*radius) + radius;
     	temp_y = Math.random() * (dim-2*radius) + radius;
 
-    	// Break out if we are not 
-    	if(particles.every(is_collided(temp_x,temp_y))){
+    	// Break out if we are not blocked
+    	if(particles.every(is_unblocked(temp_x,temp_y))){
     		break;
     	}
     }
 
+    // Set the position, radius, and velocity
     this.x = temp_x;
     this.dx = Math.random()*2 - 1;
 
@@ -57,40 +49,56 @@ var Particle = function (){
 
 // Update the density text label
 function update_dens_label(n){
-    d3.select("#density").attr("value",n);
-    d3.select("#density_value").text(n);
+  d3.select("#density").attr("value",n);
+  d3.select("#density_value").text(n);
 }
 
 // Update the list of particles
 function update_particle_list(n){
-    // We are adding particles
-    while(n > particles.length){
-        particles.push(new Particle());
-    }
+  // We are adding particles
+  while(n > particles.length){
+      particles.push(new Particle());
+  }
 
-    // We are removing particles
-    while(n < particles.length){
-        particles.pop();
-    }
+  // We are removing particles
+  while(n < particles.length){
+      particles.pop();
+  }
 }
 
+// Draw the points on the canvas
 function redraw_particles(){
-    points = frame.selectAll("circle")
-                  .data(particles)
+  points = frame.selectAll("circle")
+                .data(particles)
 
-    points.enter()
-          .append("circle")
-          .attr("cx",function(d){return d.x})
-          .attr("cy",function(d){return d.y})
-          .attr("r",function(d){return d.r})
-          .attr("class","regular")
-          .on("click", function(d) { 
-          	d3.select(this).attr("class","selected");
-          })
-    
-    points.exit()
-          .remove();
+  points.enter()
+        .append("circle")
+        .attr("cx",function(d){return d.x})
+        .attr("cy",function(d){return d.y})
+        .attr("r",function(d){return d.r})
+        .classed("regular",true)
+        .on("click", function(d) {
+        	if(root == this) {
+        		d3.select(this).attr("class","regular");
+        		root = null;
+        	} else {
+            d3.select(root).attr("class","regular");
+        		d3.select(this).attr("class","selected");
+        		root = this;
+        	}
+        });
+
+  points.exit()
+        .remove();
 }
+
+// Generator to check if two elements are collided
+function is_unblocked(x,y){
+  return function(elem){
+    return (4*radius*radius) < (elem.x-x)*(elem.x-x) + (elem.y-y)*(elem.y-y);
+  }
+}
+
 
 //
 // INITIATION
@@ -106,7 +114,7 @@ frame = d3.select("body")
 d3.select("#density")
   .attr("value",n_parts)
   .call(function(){
-  	update_dens_label(this.value);
+    update_dens_label(n_parts);
   })
   .on("input",function(){
   	update_dens_label(this.value);
@@ -114,6 +122,7 @@ d3.select("#density")
   	redraw_particles();
   });
 
+// Draw the initial set of particles
 update_particle_list(n_parts);
 redraw_particles();
 
